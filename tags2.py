e0939bf6929ebc1951c48cce94944df4ada9cd3e
Nima Alikhani <nimajaune@gmail.com>
2020-02-13 20:54:01 +0100

    some fixes

--- tags2.py
+++ tags2.py
@@ -2,9 +2,10 @@ import numpy as np
 import numpy.linalg as alg
 import cv2
 import time
-#TODO: fix debug
+
 DEBUG = 0
 VECTEURS = 1
+VIDEO = 1
 
 def normalize(vecteur):
     norme = alg.norm(vecteur)
@@ -38,13 +39,18 @@ def get_distance_2_center(x_value, y_value):
 
 def get_angle_list(centroids, num, stats, labels):
     sorted_tuples = []
-    for i in range(1, num):
+    for i in range(1, num):        #The first one is the whole image
+        if stats[i][-1]<10:        #If the number of pixels is abnormal, just ignore
+            continue
         distance = get_distance_2_center(centroids[i, 0], centroids[i, 1])
         sorted_tuples.append((i, distance))
     #Permet de trier la liste selon le 2ème paramètre
     sorted_tuples.sort(key=lambda x: x[1])
-    
+    print(stats)
+
     sorted_tuples = np.array(sorted_tuples)
+    if len(sorted_tuples)==0:
+        return 0
     index_list = sorted_tuples[:, 0]
     index_list = index_list.astype(int)
 
@@ -53,8 +59,8 @@ def get_angle_list(centroids, num, stats, labels):
         object_img = np.uint8(labels == i)
         if DEBUG:
             object_img2 = cv2.bitwise_and(image_tronquee, image_tronquee, mask=object_img)
-            cv2.imshow('imageTronquee',object_img2)
-            cv2.waitKey(2000)
+            cv2.imshow('imageTronquee', object_img2)
+            cv2.waitKey(1000)
         moment = cv2.moments(object_img, True)
         if moment["m00"] != 0:
             x = int(moment["m10"] / moment["m00"])
@@ -64,37 +70,59 @@ def get_angle_list(centroids, num, stats, labels):
 
         theta = np.arctan2(vecteur_propre_1_normalized[1],vecteur_propre_1_normalized[0]) * 180 / np.pi
         if VECTEURS:
-            #TODO: couleur vecteurs
-            cv2.arrowedLine(mask, (int(x), int(y)), (int(x + vecteur_propre_1_normalized[0]*100), int(y+vecteur_propre_1_normalized[1]*100)),  (0, 255, 255), 10)        
-            cv2.arrowedLine(mask, (int(x), int(y)), (int(x + vecteur_propre_2_normalized[0]*50), int(y+vecteur_propre_2_normalized[1]*50)),  (0, 255, 255), 10)
-            cv2.imshow('imageTronquee',mask)
-            cv2.waitKey(5000)
+            cv2.arrowedLine(img, (int(x), int(y)), (int(x + vecteur_propre_1_normalized[0]*100), int(y+vecteur_propre_1_normalized[1]*100)),  (0, 255, 255), 5)        
+            cv2.arrowedLine(img, (int(x), int(y)), (int(x + vecteur_propre_2_normalized[0]*50), int(y+vecteur_propre_2_normalized[1]*50)),  (0, 255, 255), 5)
+            cv2.imshow('Debug vecteurs', img)
+            cv2.waitKey(1000)
         angle = theta
     return(angle)
     
 
-'''
-capture = cv2.VideoCapture(0)
-while capture.isOpened():
-'''
-img = cv2.imread("test55.jpg")
-height, width = img.shape[0], img.shape[1]
+if VIDEO:
+    #TODO: fix affichage vecteurs
+    capture = cv2.VideoCapture(0)
+    while capture.isOpened():
+        #img = cv2.imread("test55.jpg")
+        _, img = capture.read()
+        height, width = img.shape[0], img.shape[1]
 
-#Downsampling
-W = width/2
-imgScale = W/width
-newX,newY = img.shape[1]*imgScale, img.shape[0]*imgScale
-img = cv2.resize(img,(int(newX),int(newY)))
+        #Downsampling
+        W = width/2
+        imgScale = W/width
+        newX,newY = img.shape[1]*imgScale, img.shape[0]*imgScale
+        img = cv2.resize(img,(int(newX),int(newY)))
 
-#Floutage
-kernel = np.ones((5,5),np.float32)/25
-img = cv2.filter2D(img,-1,kernel)
+        #Floutage
+        kernel = np.ones((5,5),np.float32)/25
+        img = cv2.filter2D(img,-1,kernel)
 
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-lower_v = np.array([0, 0, 0])
-upper_v = np.array([255, 255, 45])
-mask = cv2.inRange(hsv, lower_v, upper_v)
-#image_tronquee = cv2.bitwise_and(img, img, mask=mask)    
-num, labels, stats, centroids = cv2.connectedComponentsWithStats(mask, connectivity=8)
-angle = get_angle_list(centroids, num, stats, labels)
-print(angle)
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        lower_v = np.array([0, 0, 0])
+        upper_v = np.array([255, 255, 45])
+        mask = cv2.inRange(hsv, lower_v, upper_v)
+        #image_tronquee = cv2.bitwise_and(img, img, mask=mask)    
+        num, labels, stats, centroids = cv2.connectedComponentsWithStats(mask, connectivity=8)
+        angle = get_angle_list(centroids, num, stats, labels)
+        print(angle)
+else:
+    img = cv2.imread("test55.jpg")
+    height, width = img.shape[0], img.shape[1]
+
+    #Downsampling
+    W = width/2
+    imgScale = W/width
+    newX,newY = img.shape[1]*imgScale, img.shape[0]*imgScale
+    img = cv2.resize(img,(int(newX),int(newY)))
+
+    #Floutage
+    kernel = np.ones((5,5),np.float32)/25
+    img = cv2.filter2D(img,-1,kernel)
+
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    lower_v = np.array([0, 0, 0])
+    upper_v = np.array([255, 255, 20])
+    mask = cv2.inRange(hsv, lower_v, upper_v)
+    image_tronquee = cv2.bitwise_and(img, img, mask=mask)    
+    num, labels, stats, centroids = cv2.connectedComponentsWithStats(mask, connectivity=8)
+    angle = get_angle_list(centroids, num, stats, labels)
+    print(angle)
