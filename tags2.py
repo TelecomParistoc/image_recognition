90df35514eabf9b3b83f43adde675c33999380c8
Nima Alikhani <nimajaune@gmail.com>
2020-06-01 03:42:21 +0200

    refactoring method 2

--- tags2.py
+++ tags2.py
@@ -5,10 +5,38 @@ import time
 import random as rng
 
 rng.seed(12345)
-
 DEBUG = 1
 VECTEURS = 1
 MIN_PIXEL = 3000
+
+def get_orientation(path : str) -> int:
+    """Get the orientation from a saved image
+
+    :param path: path of the image taken by the camera
+    :type path: str
+    :return: orientation (1 = NORTH / 0 = SOUTH)
+    :rtype: int
+    """
+    
+    #Load image
+    img = cv2.imread(path)
+    img = cv2.resize(img, (360, 640), interpolation = cv2.INTER_AREA)
+    height, width = img.shape[0], img.shape[1]
+
+    #Blur
+    kernel = np.ones((5,5),np.float32)/25
+    img = cv2.filter2D(img,-1,kernel)
+
+    #Filter
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    lower_v = np.array([0, 0, 0])
+    upper_v = np.array([255, 255, 120])
+    mask = cv2.inRange(hsv, lower_v, upper_v)
+
+    #Generate connected components, then calculate the angle
+    num, labels, stats, centroids = cv2.connectedComponentsWithStats(mask, connectivity=4)
+    angle = get_orientation_from_data_final(centroids, num, stats, labels, height, width)
+    return angle
 
 def normalize(vecteur):
     norme = alg.norm(vecteur)
@@ -36,11 +64,11 @@ def get_inertia_matrix(moments):
     return matrice_inertie
 
 
-def get_distance_2_center(x_value, y_value):
+def get_distance_2_center(x_value, y_value, height, width):
     return (x_value - width / 2)**2 + (y_value - height / 2)**2
 
 
-def get_angle(centroids, num, stats, labels) -> int:
+def get_orientation_from_data(centroids, num, stats, labels, height, width, img, image_tronquee) -> int:
 
     sorted_tuples = []
 
@@ -49,7 +77,7 @@ def get_angle(centroids, num, stats, labels) -> int:
         #If the number of pixels is too low, just ignore
         if stats[i][-1] < MIN_PIXEL:
             continue
-        distance = get_distance_2_center(centroids[i, 0], centroids[i, 1])
+        distance = get_distance_2_center(centroids[i, 0], centroids[i, 1], height, width)
         sorted_tuples.append((i, distance))
 
     #Get the tuples (index of connected components, distance to center), now sorted by increasing distance
@@ -111,9 +139,62 @@ def get_angle(centroids, num, stats, labels) -> int:
             cv2.imshow('Debug vecteurs', img)
             cv2.waitKey(1000)
 
-    return(angle)
+    return(aa)
     
+def get_orientation_from_data_final(centroids, num, stats, labels, height, width) -> int:
+
+    sorted_tuples = []
+
+    #Stats of len num : num connected components detected by cv
+    for i in range(1, num):     #The first one is the whole image
+        #If the number of pixels is too low, just ignore
+        if stats[i][-1] < MIN_PIXEL:
+            continue
+        distance = get_distance_2_center(centroids[i, 0], centroids[i, 1], height, width)
+        sorted_tuples.append((i, distance))
+
+    #Get the tuples (index of connected components, distance to center), now sorted by increasing distance
+    sorted_tuples.sort(key=lambda x: x[1])
+    sorted_tuples = np.array(sorted_tuples)
+
+    #If no element, some parameters have to be tuned ... (Masking step)
+    if len(sorted_tuples)==0:
+        return np.NaN
+
+    #Get the list of indexes, still sorted by increasing distance to center
+    index_list = sorted_tuples[:, 0]
+    index_list = index_list.astype(int)
+
+    #For loop for test purpose (displaying multiple proposals, tune the MIN_PIXEL parameter zb)
+    angle = 0
+    for i in index_list[0:1]:
+        #Get the mask with the coresponding connected component
+        object_img = np.uint8(labels == i)
+
+        contours, _ = cv2.findContours(object_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        # Find the convex hull object for each contour
+        hull_list = []
+        for i in range(len(contours)):
+            hull = cv2.convexHull(contours[i])
+            hull_list.append(hull)
+
+        #Get the moments and build the inertial matrix from it, and recover center coord of the connected component
+        moment = cv2.moments(hull_list[0])
+        if moment["m00"] != 0:
+            x = int(moment["m10"] / moment["m00"])
+            y = int(moment["m01"] / moment["m00"])
+        matrice_inertie = get_inertia_matrix(moment)
+
+        #Eigenelements are calculated, they allow us to recover the angle
+        vecteur_propre_1_normalized, vecteur_propre_2_normalized = eigen_elements(matrice_inertie)
+
+        if vecteur_propre_2_normalized[1] > 0:
+            return 0
+        else:
+            return 1
+
 if __name__ == "__main__":
+    print(get_orientation("test3.jpg"))
     #Load image
     img = cv2.imread("test3.jpg")
     img = cv2.resize(img, (360, 640), interpolation = cv2.INTER_AREA)
@@ -137,6 +218,5 @@ if __name__ == "__main__":
 
     #Generate connected components, then calculate the angle
     num, labels, stats, centroids = cv2.connectedComponentsWithStats(mask, connectivity=4)
-    angle = get_angle(centroids, num, stats, labels)
+    angle = get_orientation_from_data(centroids, num, stats, labels, height, width, img, image_tronquee)
     print(angle)
-    input()
